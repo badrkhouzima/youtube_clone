f7b9ce82f1a4e6383b434f3e71c65b472f575f20
padreno <badrhand@gmail.com>
2021-10-28 12:32:01 +0200

    adding react app

---
+++ youtube-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+        hello there
+      </header>
+    </div>
+  );
+}
+
+export default App;
